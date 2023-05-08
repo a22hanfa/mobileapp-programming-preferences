753fc3cfd854738c11a0236bcd05b9fd248e43de
Hannes <12446977+glazedegg@users.noreply.github.com>
2023-05-08 14:03:12 +0200

    - Created a second activity - Layout for the activities - A button for each activity, that can switch between activities

--- app/src/main/java/com/example/project/SecondActivity.java
+++ app/src/main/java/com/example/project/SecondActivity.java
@@ -5,20 +5,19 @@ import android.os.Bundle;
 import android.widget.Button;
 
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.appcompat.widget.Toolbar;
 
-public class MainActivity extends AppCompatActivity {
+public class SecondActivity extends AppCompatActivity {
 
-    @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_second);
 
         Button button = findViewById(R.id.button);
         button.setOnClickListener(view -> {
-            Intent intent = new Intent(MainActivity.this, SecondActivity.class);
+            Intent intent = new Intent(SecondActivity.this, MainActivity.class);
             startActivity(intent);
         });
+
     }
 
 }
