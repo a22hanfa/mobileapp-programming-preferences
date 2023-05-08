deea0a245bf2ea2da41370555f1985a627737f73
Hannes <12446977+glazedegg@users.noreply.github.com>
2023-05-08 15:34:33 +0200

    - Add a SecondActivity with layout - Create buttons to switch between MainActivity and SecondActivity - Wrote a report - Methods for transferring data between activities.

--- app/src/main/java/com/example/project/MainActivity.java
+++ app/src/main/java/com/example/project/MainActivity.java
@@ -1,24 +1,37 @@
 package com.example.project;
 
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.widget.Button;
+import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 
 public class MainActivity extends AppCompatActivity {
 
+    public TextView textView;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        textView = (TextView) findViewById(R.id.textView);
 
         Button button = findViewById(R.id.button);
         button.setOnClickListener(view -> {
             Intent intent = new Intent(MainActivity.this, SecondActivity.class);
             startActivity(intent);
         });
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+        SharedPreferences sharedPreferences = getSharedPreferences(SecondActivity.changingString, MODE_PRIVATE);
+        String textFromEditText = sharedPreferences.getString(SecondActivity.changingString, "");
+        textView.setText(textFromEditText);
     }
 
 }
--- app/src/main/java/com/example/project/SecondActivity.java
+++ app/src/main/java/com/example/project/SecondActivity.java
@@ -1,23 +1,42 @@
 package com.example.project;
 
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.util.Log;
 import android.widget.Button;
+import android.widget.EditText;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 public class SecondActivity extends AppCompatActivity {
 
+    public static final String changingString = "Shared Preference";
+    EditText editText;
+
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_second);
 
-        Button button = findViewById(R.id.button);
-        button.setOnClickListener(view -> {
+        editText = (EditText) findViewById(R.id.edit_text_input);
+
+        Button backButton = findViewById(R.id.button);
+        backButton.setOnClickListener(view -> {
             Intent intent = new Intent(SecondActivity.this, MainActivity.class);
             startActivity(intent);
         });
 
+        Button applyButton = findViewById(R.id.applyButton);
+        applyButton.setOnClickListener(view -> {
+            saveData();
+        });
+
+    }
+
+    public void saveData() {
+        SharedPreferences sharedPreferences = getSharedPreferences(changingString, MODE_PRIVATE);
+        SharedPreferences.Editor editor = sharedPreferences.edit();
+        editor.putString(changingString, editText.getText().toString()).apply();
     }
 
 }
